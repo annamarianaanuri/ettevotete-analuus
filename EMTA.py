a7951b0d4c5ccbf895b7d26a2ab219a2cecfd616
annamarianaanuri <annamarianaanuri@gmail.com>
2026-04-17 12:00:51 +0300

    minor updates

--- EMTA.py
+++ EMTA.py
@@ -1,23 +1,14 @@
-# VAJALIKUD LISAD KIRJUTAMISEKS
 import streamlit as st
 import pandas as pd
 import duckdb
-
-
-# VAJALIKUD LISAD GRAAFIKUTE TEGEMISEKS
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-
-# LEHE ESIMENE PEALKIRI
-st.write("# Ettevõtluse statistika maakondade lõikes")
-
-
-# DEFINEERIME, KUST ANDMED TULEVAD
 data = pd.read_csv("emta_data.csv")
 
-
-# FILTRITE TEGEMINE, 2 TK KÕRVUTI, VÕTAB AINULT NEED READ, MILLE KOHTA ON ANDMED OLEMAS
+st.write("# Ettevõtluse statistika maakondade lõikes")
+"""
+"""
 col1, col2 = st.columns(2)
 with col1:
     aasta = st.selectbox("Aasta", options=sorted(data["aasta"].unique(), reverse=True))
@@ -27,8 +18,6 @@ with col2:
         options=duckdb.sql(f""" SELECT DISTINCT kvartal FROM data WHERE aasta = {aasta} ORDER BY kvartal DESC""")
         )
 
-
-# MUUTUJA DEFINEERIMINE
 count_by_county = duckdb.sql(f""" 
     SELECT
         maakond, 
@@ -39,31 +28,36 @@ count_by_county = duckdb.sql(f"""
     HAVING maakond IS NOT NULL
     ORDER BY ettevotete_arv DESC
 """).df()
-
-
-# ESIMESE GRAAFIKU PEALKIRI
+"""
+"""
+"""
+"""
 st.write("### Ettevõtete arv maakondade lõikes / interaktiivne")
-
-# ESIMENE GRAAFIK / INTERAKTIIVNE
+"""
+"""
 st.bar_chart(count_by_county, y="ettevotete_arv", x="maakond", sort=False, horizontal=True)
-
-
-# TEISE GRAAFIKU PEALKIRI
+"""
+"""
+"""
+"""
 st.write("### Ettevõtete arv maakondade lõikes")
-
-# TEINE GRAAFIK
+"""
+"""
 fig = plt.figure(figsize=(14,5))
 sns.barplot(count_by_county, y="ettevotete_arv", x="maakond")
 st.pyplot(fig)
-
-
-# KOLMANDA GRAAFIKU PEALKIRI
+"""
+"""
+"""
+"""
 st.write("### Ettevõtete arv maakonna & kov lõikes")
-
-# KOLMANDA GRAAFIKU FILTER
+"""
+"""
 Maakond: str = st.selectbox("maakond", options=data["maakond"].unique())
-
-# KOLMAS GRAAFIK
+"""
+"""
+"""
+"""
 st.write(duckdb.sql(f"""
     SELECT
         kov,
--- pages/Palgad.py
+++ pages/Palgad.py
@@ -1,27 +1,15 @@
-# VAJALIKUD LISAD KIRJUTAMISEKS
 import streamlit as st
 import pandas as pd
 import duckdb
-
-
-# VAJALIKUD LISAD GRAAFIKUTE TEGEMISEKS
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-
-# LEHE ESIMENE PEALKIRI
-st.write("# Palgad tööstusharude lõikes")
-
-# DEFINEERIME, KUST ANDMED TULEVAD
 data = pd.read_csv("emta_data.csv") 
 
-
-# MUUTUJA DEFINEERIMINE
+st.write("# Palgad tööstusharude lõikes")
+"""
+"""
 TAX_PERCENTAGE = 0.338 
-
-
-# DEFINEERIME TEGEVUSALA KESKMISE PALGA
-
 salary_stats = duckdb.sql(f""" 
     SELECT 
         tegevusala,
@@ -31,35 +19,21 @@ salary_stats = duckdb.sql(f"""
     GROUP BY tegevusala
     ORDER BY keskmine_palk DESC
     """).df()
-
 """
 """
 """
 """
-
-# ESIMESE GRAAFIKU PEALKIRI
-
 st.write("### Tegevusala keskmine palk töötaja kohta")
 
 """
 """
-
-# TEGEVUSALA KESKMINE PALK TÖÖTAJA KOHTA
-
 st.write(salary_stats)
-
 """
 """
 """
 """
-
-# TEISE GRAAFIKU PEALKIRI
-
 st.write("### Tegevusala keskmine palk töötaja kohta / interaktiivne")
 
 """
 """
-
-# TEGEVUSALA KESKMINE PALK GRAAFIK
-
 st.bar_chart(salary_stats, x="tegevusala", y="keskmine_palk", sort=False, horizontal=True)
